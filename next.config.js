bb43b70930b66013f0e7d27989ae562122cc47a8
Shubham <shubhamchandra067@gmail.com>
2021-10-31 14:19:06 +0530

    added aws s3 domain for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ["127.0.0.1", "ecart-server.herokuapp.com"],
+    domains: [
+      "127.0.0.1",
+      "ecart-server.herokuapp.com",
+      "ecart-server.s3.amazonaws.com",
+    ],
   },
 };
